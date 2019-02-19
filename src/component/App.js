a10a2f60ee4b9fc770aadd950bc95285a767488a
osbornetunde <osbornetunde@gmail.com>
2019-02-20 00:10:42 +0100

    feature(search bar): -search bar for searching for item -returns number of search item [Delivers #164089861]

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import SearchBar from './SearchBar'
+import SearchBar from './SearchBar';
+import unsplash from '../apis/unsplash';
 
-const App = () => {
 
-	return <div>
-				<SearchBar />
+class App extends React.Component {
+	state = { images: [] };
+
+
+	 handleSearchSubmit = async (term) => {
+		const response = await unsplash.get('/search/photos', {
+			params: { query : term }	
+		});
+
+		this.setState({ images: response.data.results });
+	}
+
+
+	render() {
+	return (
+		<div className="ui container" style={{ marginTop: '10px'}}>
+				<SearchBar onSubmit={this.handleSearchSubmit}/>
+				Found: {this.state.images.length} images
 			</div>
+	)
+}
 }
 
 export default App;
