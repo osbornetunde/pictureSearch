84d0d58dfafa429892d112b1ab3f4c0cad391ff5
osbornetunde <osbornetunde@gmail.com>
2019-02-20 01:39:02 +0100

    Updates

---
+++ precache-manifest.385a9a06fef945b9bac9b78b3d0d7333.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bb143eae8ef4d89f999",
+    "url": "/pictureSearch/static/js/runtime~main.3bb143ea.js"
+  },
+  {
+    "revision": "a3d8d81a70fbf5342104",
+    "url": "/pictureSearch/static/js/main.a3d8d81a.chunk.js"
+  },
+  {
+    "revision": "5918dbad8567dc79d88f",
+    "url": "/pictureSearch/static/js/2.5918dbad.chunk.js"
+  },
+  {
+    "revision": "a3d8d81a70fbf5342104",
+    "url": "/pictureSearch/static/css/main.073cfa71.chunk.css"
+  },
+  {
+    "revision": "daab43d53f06a505c757d0d751161e4f",
+    "url": "/pictureSearch/index.html"
+  }
+];
